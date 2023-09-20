446c08fa1d612e7993c9ccc58d3e20dc04db6a5b
Wes Hofmann <whofmann@path2response.com>
2023-09-20 14:37:07 -0500

    have a working initialization now

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,17 +1,27 @@
-import supertest from "supertest";
-import { server } from "./index";
+import { Cell, Row, Board, initialize_cell, initialize_row, initialize_board } from './index'; // import your functions
 
-describe("Server", function () {
-  const request = supertest.agent(server);
+describe('Game of Life Initialization', () => {
 
-  afterAll((done) => {
-    server.close(done);
+  test('initialize_cell should return a boolean', () => {
+    expect(typeof initialize_cell()).toBe('boolean');
   });
 
-  it("should get /", async () => {
-    const res = await request.get("/");
-    
-    expect(res.status).toBe(200)
-    expect(res.body).toEqual({"data": "It Works!"})
+  test('initialize_row should return an array of given width', () => {
+    const width = 5;
+    const row = initialize_row(width);
+    expect(row.length).toBe(width);
+    // Check if all elements are boolean
+    expect(row.every((cell: Cell) => typeof cell === 'boolean')).toBe(true);
   });
+
+  test('initialize_board should return a 2D array of given dimensions', () => {
+    const width = 5;
+    const height = 5;
+    const board = initialize_board(width, height);
+    expect(board.length).toBe(height);
+    expect(board.every((row: Row) => row.length === width)).toBe(true);
+    // Check if all elements are arrays containing booleans
+    expect(board.every((row: Row) => row.every((cell: Cell) => typeof cell === 'boolean'))).toBe(true);
+  });
+
 });
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
-// src/index.ts
+export type Cell = boolean;  // true for alive, false for dead
+export type Row = Cell[];
+export type Board = Cell[][];
 
-import http from "http";
 
-export const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(
-    JSON.stringify({
-      data: "It Works!",
-    })
-  );
-});
+export const initialize_cell = (): Cell => {
+  return Math.random() < 0.5;
+};
 
-server.listen(3000, () => {
-  console.log("Server running on http://localhost:3000/");
-});
+// tail-recursive implementation of row initialization, even though TS doesn't optimize for tail-recursion.
+// why?  because tail-recursion is awesome.  'nuff said.
+export const initialize_row = (width: number, row: Row = []): Row => {
+  if (width <= 0) return row;
+  const new_row = row.concat(initialize_cell());
+  return initialize_row(width - 1, new_row);
+};
+
+// now we will initialize the board recursively, of course.  In fact, also tail-recursively!
+export const initialize_board = (width: number, height: number, board: Board = []): Board => {
+  if (height <= 0) return board;
+  const new_board = board.concat([initialize_row(width)]);
+  return initialize_board(width, height - 1, new_board);
+};
+
