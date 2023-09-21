516e8447f752c9a69c7e48a5358aef095aadf56e
Wes Hofmann <whofmann@path2response.com>
2023-09-21 16:10:37 -0500

    Add only_a_flesh_wound_percent parameter.
    If the board's state is identical to its state a set number of steps
    ago, we assume the board has essentially become static, so we introduce
    a `only_a_flesh_wound_percent` probaility for a cell to remain alive
    even if it should have died.  We will keep increasing this probability
    as long as we still seem to be stuck in this pattern.  Once we break out
    of the pattern, we set that probabiliity back to 0.
    
    Also, we created a cell_updater_generator() function that accepts a
    only_a_flesh_wound_percent parameter and creates a cell_updater()
    function that can be used that includes that paramter.

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,10 @@ export type CellEvaluator = (
   last_board: Board
 ) => Cell;
 
+export type CellEvaluatorGenerator = (
+  only_a_flesh_wound_percent: number
+) => CellEvaluator;
+
 
 const HEIGHT   = process.stdout.rows - 3;
 const WIDTH    = process.stdout.columns - 2;
@@ -17,26 +21,34 @@ const log_file = './game.log';
 
 const log = fs.createWriteStream(log_file);
 
+// this will generate a cell_updater function that has a set percentage chance of a cell staying alive
+export const cell_updater_generator: CellEvaluatorGenerator = (only_a_flesh_wound_percent) => {
+  // This is a CellEvaluator function that implement's the rules of Conway's Game of Life. 
+  // It assigns a boolan value for a given cell based on the state of the cell's neighbors.
+  const cell_updater: CellEvaluator = (x, y, last_board) => {
+    const neighbors = [
+      [-1, -1], [-1, 0], [-1, 1],
+      [0, -1],           [0, 1],
+      [1, -1], [1, 0], [1, 1],
+    ];
+    const living_neighbors = neighbors.reduce((count, [dy, dx]) => {
+      const nx = x + dx, ny = y + dy;
+      return count + (last_board[ny]?.[nx] ? 1 : 0);
+    }, 0);
+    return (
+      (living_neighbors === 3) || 
+      (living_neighbors === 2 && last_board[y][x]) ||
+      (Math.random() < only_a_flesh_wound_percent && last_board[y][x])    // allow the possibility for a cell to randomly stay alive
+    );
+  };
+  return cell_updater;
+};
+
 
 // This is a special "initializer" CellEvaluator function.  Essentially it just assigns 
 // a random boolan value for a given cell in the board.  It ignores the last_board parameter.
 export const cell_initializer: CellEvaluator = (x, y, last_board) => {
   return Math.random() < 0.5;
-};
-
-// This is a CellEvaluator function that implement's the rules of Conway's Game of Life. 
-// It assigns a boolan value for a given cell based on the state of the cell's neighbors.
-export const cell_updater: CellEvaluator = (x, y, last_board) => {
-  const neighbors = [
-    [-1, -1], [-1, 0], [-1, 1],
-    [0, -1],           [0, 1],
-    [1, -1], [1, 0], [1, 1],
-  ];
-  const living_neighbors = neighbors.reduce((count, [dy, dx]) => {
-    const nx = x + dx, ny = y + dy;
-    return count + (last_board[ny]?.[nx] ? 1 : 0);
-  }, 0);
-  return (living_neighbors === 3) || (living_neighbors === 2 && last_board[y][x]);
 };
 
 
@@ -101,21 +113,58 @@ export const render_board = (board: Board) => {
 };
 
 
+// Function to compare two boards for equality
+export const are_boards_equal = (board1: Board, board2: Board): boolean => {
+  if (board1.length !== board2.length) return false;
+  
+  for (let i = 0; i < board1.length; i++) {
+      if (board1[i].length !== board2[i].length) return false;
+      
+      for (let j = 0; j < board1[i].length; j++) {
+          if (board1[i][j] !== board2[i][j]) return false;
+      }
+  }
+  return true;
+};
+
+
 // This function is our main game loop.  It will initialize the board, and then update and render
 export const start_game = () => {
   let board: Board = update_board(WIDTH, HEIGHT, cell_initializer);
+
+  let prev_board_count = 6;
+  let prev_boards: Board[] = [];
+
+  let only_a_flesh_wound_percent = 0;
+
+  let cell_updater: CellEvaluator = cell_updater_generator(only_a_flesh_wound_percent); 
 
   // clear the terminal/screen
   process.stdout.write('\u001b[2J\u001b[0;0H');
 
   const loop = setInterval(() => {
     render_board(board);
-    log.write('-------------------\n');
-    log.write('Last Board:' + JSON.stringify(board) + '\n');
 
+    // compare the currrent board with our previous boards
+    if (prev_boards.some((prev_board) => are_boards_equal(board, prev_board))) {
+      only_a_flesh_wound_percent += 0.00001;
+      cell_updater = cell_updater_generator(only_a_flesh_wound_percent);
+      log.write(`Board was static for ${prev_board_count} steps.  Setting only_a_flesh_wound_percent to ${only_a_flesh_wound_percent}\n`);
+    } else {
+      if (only_a_flesh_wound_percent > 0) {
+        only_a_flesh_wound_percent = 0;
+        cell_updater = cell_updater_generator(only_a_flesh_wound_percent);
+        log.write(`Resetting only_a_flesh_wound_percent to ${only_a_flesh_wound_percent}\n`);
+      }
+    }
+
+    // update previous boards for next iteration
+    if (prev_boards.length >= prev_board_count) prev_boards.shift();
+    prev_boards.push(board);
+
+    // now generate new board
     board = update_board(WIDTH, HEIGHT, cell_updater, board);
-    log.write('New Board:' + JSON.stringify(board) + '\n');
-  }, 100);
+  }, 50);
 };
 
 
